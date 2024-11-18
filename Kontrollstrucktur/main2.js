bb8104bbcc52a46cb6507c7d4a26d18523b6d509
PsychoZwergII <leon.egli@student.ipso.ch>
2024-11-18 16:13:47 +0100

    Tag 2

--- Kontrollstrucktur/main2.js
+++ Kontrollstrucktur/main2.js
@@ -3,10 +3,28 @@ let firstClick = true;
 
 document.getElementById("ageButton").addEventListener("click", function () {
   age++;
+
   if (firstClick) {
     firstClick = false;
     document.getElementById("ageDisplay").innerText = "Du bist noch so jung";
   } else {
-    document.getElementById("ageDisplay").innerText = "Du bist " + age + " alt";
+    let message = "Du bist " + age + " Jahre alt";
+
+    if (age >= 15 && age < 18) {
+      message += ", du darfst bis 22 Uhr raus.";
+    } else if (age >= 18) {
+      message += ", du darfst alles.";
+
+      if (age >= 25) {
+        message += ", Du bist alkoholiker";
+      } else if (age >= 65) {
+        message += ", Du hast kein Geld mehr zum Leben";
+        if (age >= 80) {
+          message += ", Du bist ein alter Mann";
+        }
+      }
+    }
+
+    document.getElementById("ageDisplay").innerText = message;
   }
 });
